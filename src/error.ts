af9dfb49b8923b136317a3abf56a3b9399ec442a
Danielle-Luz <daniln2209@gmail.com>
2023-02-15 00:29:02 -0300

    feat: add zod error

--- src/error.ts
+++ src/error.ts
@@ -1,30 +1,25 @@
-import { iErrorMessage } from './interfaces/index';
-import { Request, Response } from "express"
+import { iErrorMessage } from "./interfaces/index";
+import { Request, Response } from "express";
+import { ZodError } from "zod";
 
 class AppError extends Error {
   statusCode: number;
 
   constructor(message: string, statusCode: number) {
     super(message);
-    
+
     this.statusCode = statusCode;
   }
 }
 
 export const errorHandler = (error: Error, req: Request, res: Response) => {
-  const errorMessage: iErrorMessage = {
-    statusCode: 500,
-    message: "Internal server error"
-  }
-
   if (error instanceof AppError) {
     const { statusCode, message } = error;
 
-    errorMessage.statusCode = statusCode;
-    errorMessage.message = message;
-
-    return res.status(statusCode).send(errorMessage);
+    return res.status(statusCode).send({ message });
+  } else if (error instanceof ZodError) {
+    return res.status(400).send({ message: error.flatten().fieldErrors });
   }
 
-  return res.status(500).send(errorMessage);
-}
+  return res.status(500).send({ message: "Internal server error" });
+};
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,3 @@
 export interface iErrorMessage {
-  statusCode: number;
   message: string;
 }
