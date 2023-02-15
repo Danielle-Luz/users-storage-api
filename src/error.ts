6324120596292cc6fa0390a9dead975dd318b24f
Danielle-Luz <daniln2209@gmail.com>
2023-02-14 21:36:42 -0300

    feat: create AppError class

--- src/error.ts
+++ src/error.ts
@@ -1,5 +1,15 @@
 import { Request, Response } from "express"
 
+class AppError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    
+    this.statusCode = statusCode;
+  }
+}
+
 export const errorHandler = (error: Error, req: Request, res: Response) => {
-  
+
 }
